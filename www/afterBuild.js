7e5261716b46c6f2c11366a5ef9d188cdfbed786
Muhammad Subhani <alisubhani@Muhammads-MacBook-Pro.local>
2021-03-28 23:04:22 +0500

    hook to copy intro.mp4 file to www folder in iOS

--- www/afterBuild.js
+++ www/afterBuild.js
@@ -4,29 +4,12 @@ const path = require('path')
 var projectRoot
 
 module.exports = function (context) {
-  console.log(context.hook + ': Importing video file')
+  console.log(context.hook + ': Importing video file', context);
 
-  projectRoot = path.resolve(path.dirname(context.scriptLocation), '..')
-  console.log('Project root directory:', projectRoot)
+  projectRoot = path.resolve(context.opts.paths[0]);
+  console.log('Projec destination directory:', projectRoot)
   //copyFile('jquery', path.join('dist', 'jquery.min.js'), path.join('www', 'js', 'res', 'jquery.min.js'))
-  fse.copySync('/videoTest2.mp4', projectRoot);
-  console.log(path.relative(projectRoot,'/videoTest2.mp4'));
-}
-
-function copyFile (npmPackage, // oficial name of the npm package from which the file is to be copied from
-  fileRelativePath, // file path with respect to the main directory of the npm package (node_modules/<package>/)
-  destFilePath) { // file's path to where it is copied, relative to the project bin/ directory
-  // trick to get the npm module main directory
-  // https://stackoverflow.com/a/49455609/1243247
-  const packageDirFullpath = path.dirname(require.resolve(path.join(npmPackage, 'package.json')))
-  const fileOriginFullPath = path.join(packageDirFullpath, fileRelativePath)
-  const fileDestFullPath = path.join(projectRoot, destFilePath)
-
-  fse.copySync(fileOriginFullPath, fileDestFullPath)
-
-  const consoleMsg = npmPackage + ': ' +
-    path.relative(projectRoot, fileOriginFullPath) + ' -> ' +
-    path.relative(projectRoot, fileDestFullPath)
-
-  console.log(consoleMsg)
+  var fileRaed = fse.readFileSync(context.opts.projectRoot+'/intro.mp4');
+  fse.writeFileSync(projectRoot+'/intro.mp4',fileRaed);
+  console.log(path.relative(projectRoot,'/intro.mp4'));
 }
