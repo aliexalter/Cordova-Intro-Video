5e1e7deeae026c50253cc2182f1e33f6ea97812b
Muhammad Subhani <alisubhani@Muhammads-MacBook-Pro.local>
2021-04-01 15:03:21 +0500

    change file name to intro.mp4

--- www/afterBuild.js
+++ www/afterBuild.js
@@ -1,15 +1,118 @@
-const fse = require('fs-extra')
-const path = require('path')
+const fse = require('fs-extra');
+const path = require('path');
+var parser = require('xml-js');
 
 var projectRoot
+var IOS_DIR = 'platforms/ios';
+var ANDROID_DIR = 'platforms/android';
+var PLATFORM;
+var _configXml;
+
+var setupEnv = function(){
+  appName = getAppName();
+  PLUGIN_ID = getPluginId();
+  PLATFORM = {
+    IOS: {
+        dest: IOS_DIR + '/' + appName + '/Resources/intro.mp4',
+        src: [
+            'intro.mp4',
+            IOS_DIR + '/www/intro.mp4',
+            'www/intro.mp4'
+        ]
+    },
+    /*ANDROID: {
+        dest: ANDROID_DIR + '/app/google-services.json',
+        src: [
+            'google-services.json',
+            ANDROID_DIR + '/assets/www/google-services.json',
+            'www/google-services.json',
+            ANDROID_DIR + '/app/src/main/google-services.json'
+        ],
+        colorsXml: {
+            src: './plugins/' + utilities.getPluginId() + '/src/android/colors.xml',
+            target: ANDROID_DIR + '/app/src/main/res/values/colors.xml'
+        },
+        performanceGradlePlugin: {
+            classDef: 'com.google.firebase:perf-plugin',
+            pluginDef: 'com.google.firebase.firebase-perf'
+        }
+    }*/
+  };
+}
 
 module.exports = function (context) {
-  console.log(context.hook + ': Importing video file', context);
+  console.log(context.hook + ': Importing video file', context.opts.platforms);
+  var platforms = context.opts.platforms;
+  setContext(context);
+  setupEnv();
 
-  projectRoot = path.resolve(context.opts.paths[0]);
-  console.log('Projec destination directory:', projectRoot)
-  //copyFile('jquery', path.join('dist', 'jquery.min.js'), path.join('www', 'js', 'res', 'jquery.min.js'))
-  var fileRaed = fse.readFileSync(context.opts.projectRoot+'/intro.mp4');
-  fse.writeFileSync(projectRoot+'/intro.mp4',fileRaed);
-  console.log(path.relative(projectRoot,'/intro.mp4'));
+  if(platforms.indexOf('ios') !== -1 && directoryExists(IOS_DIR)){
+    copyFile(PLATFORM.IOS);
+  }
 }
+
+//Utilities
+
+setContext = function(context){
+  _context = context;
+};
+
+copyKey = function(platform){
+  for(var i = 0; i < platform.src.length; i++){
+      var file = platform.src[i];
+      if(this.fileExists(file)){
+          try{
+              var contents = fs.readFileSync(path.resolve(file)).toString();
+
+              try{
+                  var destinationPath = platform.dest;
+                  var folder = destinationPath.substring(0, destinationPath.lastIndexOf('/'));
+                  fs.ensureDirSync(folder);
+                  fs.writeFileSync(path.resolve(destinationPath), contents);
+              }catch(e){
+                  // skip
+              }
+          }catch(err){
+              console.log(err);
+          }
+
+          break;
+      }
+  }
+};
+
+fileExists = function(filePath){
+  try{
+      return fs.statSync(path.resolve(filePath)).isFile();
+  }catch(e){
+      return false;
+  }
+};
+
+directoryExists = function(dirPath){
+  try{
+      return fs.statSync(path.resolve(dirPath)).isDirectory();
+  }catch(e){
+      return false;
+  }
+};
+
+parseConfigXml = function(){
+  if(_configXml) return _configXml;
+  _configXml = parseXmlFileToJson("config.xml");
+  return _configXml;
+};
+
+parseXmlFileToJson = function(filepath, parseOpts){
+  parseOpts = parseOpts || {compact: true};
+  return JSON.parse(parser.xml2json(fs.readFileSync(path.resolve(filepath), 'utf-8'), parseOpts));
+};
+
+getAppName = function(){
+  return parseConfigXml().widget.name._text.toString().trim();
+};
+
+getPluginId = function(){
+  // if(!_context) throw "Cannot retrieve plugin ID as hook context is not set";
+  return _context.opts.plugin.id;
+};
